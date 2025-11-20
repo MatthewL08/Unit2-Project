248e75a7ebffa4bdb4834ccf3e2df3068f2f98b6
MatthewL08 <Matthewl439@nycstudents.net>
2025-11-20 08:45:03 -0500

    Add files via upload
    11/20

--- src/Class.java
+++ src/Class.java
@@ -5,47 +5,49 @@ public class Class {
     private int misc;
     private int salmon;
     private int tuna;
-    private int salmonPrice;
-    private int tunaPrice;
+    private final int salmonPrice;
+    private final int tunaPrice;
+    private String rodType;
 
-    public Class(int coins, int misc, int salmon, int tuna, int salmonPrice, int tunaPrice) {
+    public Class(int coins, int misc, int salmon, int tuna, int salmonPrice, int tunaPrice, String rod) {
         this.coins = coins;
         this.misc = misc;
         this.salmon = salmon;
         this.tuna = tuna;
         this.salmonPrice = salmonPrice;
         this.tunaPrice = tunaPrice;
+        rodType = rod;
 
     }
 
-    public void Fish(String rod) {
+    public void Fish() {
         int fish = (int) ((Math.random() * 100) + 1);
-        switch (rod) {
-            case "rodtype1" -> {
+        switch (this.rodType) {
+            case "basic" -> {
                 if (fish == 100) {
                     misc++;
                     return;
-                } else if ((fish < 100) && (fish > 50)) {
+                } else if ((fish < 100) && (fish > 90)) {
                     tuna++;
                     return;
                 }
                 salmon++;
             }
-            case "rodtype2" -> {
-                if (fish == 100) {
+            case "bamboo" -> {
+                if (fish >= 95) {
                     misc++;
                     return;
-                } else if ((fish < 100) && (fish > 50)) {
+                } else if (fish > 75) {
                     tuna++;
                     return;
                 }
                 salmon++;
             }
-            case "rodtype3" -> {
-                if (fish == 100) {
+            case "steel" -> {
+                if (fish > 80) {
                     misc++;
                     return;
-                } else if ((fish < 100) && (fish > 50)) {
+                } else if ((fish < 80) && (fish > 40)) {
                     tuna++;
                     return;
                 }
@@ -56,7 +58,6 @@ public class Class {
 
 
     public void printInfo() {
-        int catches = misc + salmon + tuna;
         System.out.println("-----------------------------------------------");
         System.out.println("coins: " + coins + " catches: Salmon:" + salmon + ", tuna: " + tuna + ", misc: " + misc);
         System.out.println("-----------------------------------------------");
@@ -65,7 +66,7 @@ public class Class {
     }
 
 
-    public int sell() {
+    public void sell() {
         int salmonProfit = salmonPrice * salmon;
         int tunaProfit = tunaPrice * tuna;
         int total = salmonProfit + tunaProfit;
@@ -73,10 +74,10 @@ public class Class {
         tuna = 0;
         coins = coins + total;
         System.out.println("You made " + total + "coins, current total is " + coins);
-        return total;
+
     }
 
-    public boolean coinFlip(String call, int wager) {
+    public void coinFlip(String call, int wager) {
         int random = (int) (Math.random() * 2 + 1);
         String coin;
         if (wager > coins) {
@@ -94,10 +95,21 @@ public class Class {
             coins = coins - wager;
             System.out.println("you lost");
         }
-        return Objects.equals(call, coin);
     }
 
-    public void market() {
-
+    public void newRod() {
+        System.out.println("Bamboo rod: misc 5%/tuna 20%/Salmon 75%");
+        System.out.println("steel rod: misc 20%/tuna 40%/Salmon 40%");
+        System.out.println();
     }
+
+    public String purchase (String material){
+    rodType = material;
+    if (material.equals("bamboo")) {
+            coins = coins - 0; }
+    else if (material.equals("steel")) {
+            coins = coins - 0; }
+        return "you have bought a " + material + " rod";
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-    Class one = new Class (0,0,0,0,1,1);
+        Class one = new Class(0, 0, 0, 0, 1, 1, "bamboo");
 
         for (int i = 1; i < 1000; i++) {
             one.printInfo();
             String action = scan.next();
-        if (action.equals("Fish")){
-            one.Fish("rodtype1");}
-        else if (action.equals("sell")){
-                one.sell();}
-        else if (action.equals("coinflip")){
-            System.out.println("How much would you like to wager");
-            int wager = scan.nextInt();
-            System.out.println("what do you call");
-            String call = scan.next();
-            one.coinFlip(call, wager);
+            switch (action) {
+                case "fish" -> one.Fish();
+                case "sell" -> one.sell();
+                case "coinflip" -> {
+                    System.out.println("How much would you like to wager");
+                    int wager = scan.nextInt();
+                    System.out.println("what do you call");
+                    String call = scan.next();
+                    one.coinFlip(call, wager);
+                }
+                case "purchase" -> {
+                    System.out.println("what type of rod");
+                    String material = scan.next();
+                    System.out.println(one.purchase(material));
+                }
+                case "market" -> one.newRod();
 
             }
-
-    }
+        }
     }
 }
